a70a714b9fe73d79e3cb972a361152d4d6c3a316
mgunter-pivotal <mgunter@pivotal.io>
2015-09-22 16:25:27 -0400

    first commit

---
+++ src/main/java/demo/Demo1MgApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1MgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1MgApplication.class, args);
+    }
+}
